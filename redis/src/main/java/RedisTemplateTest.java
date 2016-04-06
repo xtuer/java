6dbeeb0d5be10feea9fff02b2ad0a298a55102c0
xtuer <biao.mac@icloud.com>
2016-04-06 14:53:02 +0800

    Redis Demo

---
+++ redis/src/main/java/RedisTemplateTest.java
@@ -0,0 +1,31 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class RedisTemplateTest {
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
+        ApplicationContext context = new ClassPathXmlApplicationContext("redis.xml");
+        final RedisTemplate redisTemplate = context.getBean("redisTemplate", RedisTemplate.class);
+
+        redisTemplate.opsForValue().set("name", "Biao"); // 设置数据到 Redis
+        redisTemplate.opsForValue().set("nginx", "Go");
+        System.out.println(redisTemplate.keys("n*")); // 使用模式匹配查找以 n 开头的所有 key
+
+        // 每隔一秒钟从 Redis 里读取一次数据
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println((++count) + ":" + redisTemplate.opsForValue().get("name"));
+                } catch(Exception ex) {
+
+                }
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+}
