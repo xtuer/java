4f5dc8e512cab327ede1259669a73b5232dc5ed6
xtuer <biao.mac@icloud.com>
2016-04-09 17:04:42 +0800

    Revise gretty

--- template-web-gradle-mini/src/main/java/com/xtuer/controller/DemoController.java
+++ template-web-gradle-mini/src/main/java/com/xtuer/controller/DemoController.java
@@ -6,17 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
-    @RequestMapping("/")
+public class DemoController {
+    @RequestMapping("/demo")
     @ResponseBody
-    public String index() {
-        return "Welcome";
-    }
-
-    @RequestMapping("/hello")
-    public String hello(ModelMap model) {
-       model.put("name", "Biao");
-
-        return "hello.htm";
+    public String demo() {
+        System.out.println("hi---");
+        return "Welcome---+++-----";
     }
 }
