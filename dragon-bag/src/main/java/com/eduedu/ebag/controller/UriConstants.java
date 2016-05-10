36bbd4f9642ae6b1b0d9e85c853450b1df8dcca5
xtuer <biao.mac@icloud.com>
2016-05-10 13:50:25 +0800

    rename dragon-ebag

--- dragon-bag/src/main/java/com/eduedu/ebag/controller/UriConstants.java
+++
@@ -1,9 +0,0 @@
-package com.eduedu.ebag.controller;
-
-public interface UriConstants {
-    // 注销登陆
-    String URI_LOGIN  = "/login";
-    String VIEW_LOGIN = "login.html";
-    String URI_DENY   = "/deny";
-    String URI_LOGOUT = "/logout";
-}
