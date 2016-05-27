78712f96bee1a227ab6cf6bd4827662b5198eb40
xtuer <biao.mac@icloud.com>
2016-05-27 13:20:40 +0800

    write csv

---
+++ mix/src/main/java/CsvWriter.java
@@ -0,0 +1,22 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.IOException;
+
+public class CsvWriter {
+
+    public static void main(String[] args) throws IOException {
+        //Create the CSVFormat object with "\n" as a record delimiter
+        StringBuilder result = new StringBuilder();
+        CSVFormat csvFormat =  CSVFormat.DEFAULT.withRecordSeparator("\n");
+        CSVPrinter csvPrinter = new CSVPrinter(result, csvFormat);
+
+        csvPrinter.printRecord("ID", "username", "password");
+        csvPrinter.printRecord("1", "Alice", "Passw0rd");
+        csvPrinter.printRecord("2", "Bob", "123456");
+        csvPrinter.printRecord("3", "John", "Aloha");
+        csvPrinter.close();
+
+        System.out.println(result);
+    }
+}
