d64cb43449f999ccd5cec8b8f0c00ba6d6292a70
xtuer <biao.mac@icloud.com>
2016-10-11 05:40:38 +0800

    revise

--- web-mix/src/main/java/com/xtuer/controller/RestController.java
+++ web-mix/src/main/java/com/xtuer/controller/RestController.java
@@ -2,12 +2,22 @@ package com.xtuer.controller;
 
 import com.xtuer.bean.Result;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
 public class RestController {
+    @GetMapping("/rest/{id}")
+    @ResponseBody
+    public Result handleGet(@PathVariable int id, @RequestParam String name, ModelMap map) {
+        map.addAttribute("id", id);
+        map.addAttribute("name", name);
+
+        return new Result(true, "GET handled", map);
+    }
+
     @GetMapping("/rest")
     @ResponseBody
     public Result handleGet(@RequestParam String name) {
