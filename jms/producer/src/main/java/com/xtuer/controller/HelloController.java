dd13b74f0ed53ae77ab7b65c2384451dc84cc039
xtuer <biao.mac@icloud.com>
2016-11-19 18:12:19 +0800

    web mini template

--- jms/producer/src/main/java/com/xtuer/controller/HelloController.java
+++ jms/producer/src/main/java/com/xtuer/controller/HelloController.java
@@ -6,10 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class DemoController {
-    @RequestMapping("/demo")
+public class HelloController {
+    @RequestMapping("/")
     @ResponseBody
-    public String demo() {
-        return "Welcome---";
+    public String index() {
+        return "Welcome";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(ModelMap model) {
+       model.put("name", "Biao");
+
+        return "hello.htm";
     }
 }
