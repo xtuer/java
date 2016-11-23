7852ab98b392773be3c15ad6c0e307324c328305
xtuer <biao.mac@icloud.com>
2016-11-23 16:25:50 +0800

    FlatBuffers

---
+++ flatbuffer/src/test/java/TestFlatBuffer.java
@@ -0,0 +1,41 @@
+import com.google.flatbuffers.FlatBufferBuilder;
+import com.xtuer.bean.Person;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+
+public class TestFlatBuffer {
+    private static final String FILE_NAME = "/Users/Biao/Desktop/person.data";
+
+    @Test
+    public void testWrite() throws Exception {
+        // [1] 创建 Person 对象
+        FlatBufferBuilder builder = new FlatBufferBuilder();
+        int orc = Person.createPerson(builder, builder.createString("道格拉斯·狗"), 30);
+        builder.finish(orc);
+
+        // [2] 序列化 Person 对象为字节码
+        byte[] buffer = builder.sizedByteArray();
+
+        // [3] 保存字节码到文件，也可以进行网络传输
+        FileOutputStream out = new FileOutputStream(FILE_NAME);
+        out.write(buffer);
+        out.close();
+    }
+
+    @Test
+    public void testRead() throws Exception {
+        // [1] 从文件读取字节码，也可以是使用 Socket 传输得到的
+        byte[] buf = new byte[1024];
+        FileInputStream in = new FileInputStream(FILE_NAME);
+        in.read(buf);
+        in.close();
+
+        // [2] 把字节码反序列化为 Person 对象
+        ByteBuffer buffer = ByteBuffer.wrap(buf);
+        Person person = Person.getRootAsPerson(buffer);
+        System.out.println("Name: " + person.name() + ", Age: " + person.age());
+    }
+}
--- mix/src/main/java/Test.java
+++ mix/src/main/java/Test.java
@@ -1,32 +1,9 @@
 public class Test {
+    public static void foo(int x, int y) {
+        System.out.println("foo()");
+    }
+
     public static void main(String[] args) {
-        Test t = new Test();
-
-        A a1 = new A<String>() {
-            @Override
-            public void set(String s) {
-                System.out.println("A<String>: " + s);
-            }
-        };
-
-        A a2 = (A<Integer>) n -> {
-            System.out.println("A<Integer>: " + n);
-        };
-
-        t.m1(a1);
-        t.m2(a2);
-        t.m2(a1);
+        foo(2, 3);
     }
-
-    public void m1(A<String> a) {
-        a.set("ABC");
-    }
-
-    public void m2(A<Integer> a) {
-        a.set(12345);
-    }
-}
-
-interface A<T>{
-    void set(T t);
 }
