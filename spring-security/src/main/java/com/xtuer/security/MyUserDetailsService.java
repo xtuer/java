6a4e2ac1ab5c6085bf0877a58c4f2eacb082086a
xtuer <biao.mac@icloud.com>
2017-02-27 09:18:30 +0800

    revise

--- spring-security/src/main/java/com/xtuer/security/MyUserDetailsService.java
+++ spring-security/src/main/java/com/xtuer/security/MyUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.xtuer.security;
 
+import com.xtuer.bean.MyUserDetails;
 import com.xtuer.dao.UserDao;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,9 +11,9 @@ public class MyUserDetailsService implements UserDetailsService {
 
     /**
      * 使用 username 加载用户的信息，如密码，权限等
+     *
      * @param username 登陆表单中用户输入的用户名
      * @return
-     * @throws UsernameNotFoundException
      */
     @Override
     public UserDetails loadUserByUsername(String username) {
