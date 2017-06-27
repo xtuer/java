3a05f609a01bee9dc1c401c66f36b6a9a3216af1
xtuer <biao.mac@icloud.com>
2017-06-27 22:22:46 +0800

    修改配置

--- paper-suite/config.groovy
+++ paper-suite/config.groovy
@@ -1,26 +1,48 @@
-// gradle clean build -Penv=production
+// gradle clean -Denv=production assemble
 environments {
     development {
         jdbc {
             driverClassName = 'com.mysql.jdbc.Driver'
-            url = 'jdbc:mysql://localhost:3306/paper_suite?useUnicode=true&amp;characterEncoding=UTF-8'
+            url = 'jdbc:mysql://localhost:3306/shitu_paper?useUnicode=true&amp;characterEncoding=UTF-8'
             username = 'root'
             password = 'root'
         }
 
+        redis {
+            host = 'localhost'
+            port = 6379
+            password = ''
+        }
+
+        paper {
+            baseDirectory = '/Users/Biao/Documents/套卷/papers'
+            exportDirectory = '/Users/Biao/Documents/套卷/export'
+        }
+
         baseUrl = ''
-        logDir = "/temp/logs"
+        logDir = "/temp/shitu.edu-edu.com/logs"
     }
 
     production {
         jdbc {
             driverClassName = 'com.mysql.jdbc.Driver'
-            url = 'jdbc:mysql://localhost:3306/paper?useUnicode=true&amp;characterEncoding=UTF-8'
+            url = 'jdbc:mysql://127.0.0.1:3306/shitu_paper?useUnicode=true&amp;characterEncoding=UTF-8'
             username = 'root'
-            password = 'root'
+            password = 'tiger_sun'
+        }
+
+        redis {
+            host = '172.16.1.41'
+            port = 6379
+            password = '123456'
+        }
+
+        paper {
+            baseDirectory = '/data/shitu.edu-edu.com/papers'
+            exportDirectory = '/data/shitu.edu-edu.com/export'
         }
 
         baseUrl = ''
-        logDir = "/temp/logs"
+        logDir = "/temp/shitu.edu-edu.com/logs"
     }
 }
