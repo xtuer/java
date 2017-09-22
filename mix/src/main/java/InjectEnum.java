8f6d7063d35342830b5a86b68610bbac02062d66
Biao <biao.mac@icloud.com>
2017-09-22 11:54:41 +0800

    注入 enum

---
+++ mix/src/main/java/InjectEnum.java
@@ -0,0 +1,19 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@Getter
+@Setter
+public class InjectEnum {
+    private SerializerFeature features[];
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("enum.xml");
+        InjectEnum obj = context.getBean("injectEnum", InjectEnum.class);
+
+        System.out.println(JSON.toJSONString(obj));
+    }
+}
