9dc4daf16091f46ceec0f1dfa78825a05c4fe1c1
Biao <biao.mac@icloud.com>
2017-10-13 16:27:48 +0800

    test

--- mix/src/main/java/Test.java
+++ mix/src/main/java/Test.java
@@ -2,23 +2,19 @@ import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 public class Test {
     private static Logger logger = LoggerFactory.getLogger(Test.class);
     private int id;
 
     public static void main(String[] args) throws Exception {
-        if (logger.isDebugEnabled()) {
-            logger.debug("你好 {}", 1234);
-        }
-
-        foo();
-    }
-
-    public static void foo(String... strs) {
-        System.out.println(strs);
-        for (String str : strs) {
-            System.out.println(strs);
-        }
+        System.out.println(Instant.now().getEpochSecond());
+        System.out.println(System.currentTimeMillis() / 1000);
     }
 }
