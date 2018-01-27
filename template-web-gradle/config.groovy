9c2a2c67591fba25c2b4821eb68c2883f517d409
Biao <biao.mac@icloud.com>
2018-01-27 22:50:16 +0800

    让部署更灵活

--- template-web-gradle/config.groovy
+++ template-web-gradle/config.groovy
@@ -1,8 +1,15 @@
-// 打包命令: gradle clean build -Denv=production
+// 打包: gradle clean build  -Denv=production
+// 部署: gradle clean deploy -Denv=production
 
 environments {
     // 开发环境配置
     development {
+        deploy {
+            hostname = '127.0.0.1'
+            username = 'root'
+            password = 'root'
+        }
+
         database {
             url = 'jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8'
             username = 'root'
@@ -25,6 +32,12 @@ environments {
 
     // 线上环境配置
     production {
+        deploy {
+            hostname = '120.92.26.194'
+            username = 'root'
+            password = 'xxxx'
+        }
+
         database {
             url = 'jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8'
             username = 'root'
