4abd89b9f8f28b519181ee2d6cc4557e4cfb5e18
Biao <biao.mac@icloud.com>
2018-02-07 17:33:45 +0800

    美化 build.gradle

--- mix/src/main/java/BaiduOcr.java
+++ mix/src/main/java/BaiduOcr.java
@@ -9,28 +9,28 @@ import java.io.IOException;
 public class BaiduOcr {
     private static final String APP_ID  = "k364IHWiCdW1gZtL6eKfNRqM";
     private static final String APP_KEY = "2sGL1HlcoYDaStLiCrsEiNRqHbDQEWax";
-    private static final String TOKEN   = "24.2f86da893aacea8f1af0063ccdf02858.2592000.1520561223.282335-10804628"; // 30 天有效期
+    private static final String ACCESS_TOKEN = "24.2f86da893aacea8f1af0063ccdf02858.2592000.1520561223.282335-10804628"; // 30 天有效期
 
     public static void main(String[] args) throws IOException {
         String json = detectText("/Users/Biao/Desktop/y.png");
         System.out.println(json);
     }
 
-    // 识别图中的文字，返回 JSON 格式字符串
+    // 识别图中的文字，返回识别结果的 JSON 格式字符串, path 为图片的路径
     public static String detectText(String path) throws IOException {
         String image = ImageBase64Utils.imageToBase64String(path);
         int startIndex = image.indexOf(",") + 1;
         image = image.substring(startIndex); // 图片的 Base64 编码是不包含图片头的，如（data:image/jpg;base64,）
 
         String response = HttpClient.post("https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic")
-                .param("access_token", TOKEN)
+                .param("access_token", ACCESS_TOKEN)
                 .param("image", image)
                 .execute().asString();
         return response;
     }
 
-    // 使用 APP_ID + APP_KEY 换取 token
-    public static String requestToken() {
+    // 使用 APP_ID + APP_KEY 换取 access token
+    public static String requestAccessToken() {
         String response = HttpClient.post("https://aip.baidubce.com/oauth/2.0/token")
                 .param("grant_type", "client_credentials")
                 .param("client_id", APP_ID)
