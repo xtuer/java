35620351254ae1cb449a8edd1eec6e503635fe9b
Biao <biao.mac@icloud.com>
2018-03-04 17:09:58 +0800

    定时访问 MongoDB，测试自动重连

--- mongodb/src/main/java/RepositoryTest.java
+++ mongodb/src/main/java/RepositoryTest.java
@@ -3,18 +3,23 @@ import com.xtuer.repository.PersonRepository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class RepositoryTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("mongodb.xml");
         PersonRepository repository = context.getBean("personRepository", PersonRepository.class);
         // MongoTemplate template = context.getBean("mongoTemplate", MongoTemplate.class);
-        initData(repository);
+        // initData(repository);
+        testReconnect(repository);
 
         // System.out.println(repository.findAll());
         System.out.println(repository.findByFirstName("Kelly"));
         System.out.println(repository.findByLastName("Giles"));
     }
 
+    // 初始化创建数据
     public static void initData(PersonRepository repository) {
         repository.insert(new Person(1L, "Hodgson",  "Giles"));
         repository.save(new Person(2L, "Richardson", "Giles"));
@@ -25,5 +30,16 @@ public class RepositoryTest {
         repository.insert(new Person(7L, "Hood",     "David"));
         repository.insert(new Person(8L, "Walton",   "Lester"));
         repository.insert(new Person(9L, "Whitehead", "Eddy"));
+    }
+
+    // 定时访问，测试自动重连
+    public static void testReconnect(PersonRepository repository) {
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+            try {
+                System.out.println(repository.findByFirstName("Kelly"));
+            } catch (Exception ex) {
+                System.out.println("Disconnected"); // 连接断开后这里不会输出，但是如果不放在 try 里就不能继续执行了，诡异
+            }
+        }, 0, 1L, TimeUnit.SECONDS);
     }
 }
--- mongodb/src/main/java/com/xtuer/repository/PersonRepository.java
+++ mongodb/src/main/java/com/xtuer/repository/PersonRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
+/**
+ * PersonRepository 对应集合 person，会自动创建
+ */
 public interface PersonRepository extends MongoRepository<Person, Long> {
+    // Spring Data MongoDB 会自动生成下面 2 个函数
     List<Person> findByFirstName(String firstName);
     List<Person> findByLastName(String lastName);
 }
