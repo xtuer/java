027f405cd3235f30e09cee0525a0977f330b59fa
Biao <biao.mac@icloud.com>
2019-05-09 11:38:25 +0800

    bom

--- mix/src/main/java/Test.java
+++ mix/src/main/java/Test.java
@@ -1,5 +1,19 @@
 public class Test {
-    public static void main(String[] args) throws Exception {
-        System.out.println(System.getenv("JAVA_HOME"));
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 10; ++i) {
+                System.out.println(i);
+                if (i > 3) {
+                    throw new RuntimeException("Exception occurred");
+                }
+            }
+        });
+
+        // 如果不设置，就会把异常抛出给 main() 函数: 设置为 null 也可以
+        t.setUncaughtExceptionHandler((or, e) -> {
+            System.out.println(or.getName());
+        });
+
+        t.start();
     }
 }
