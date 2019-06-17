ff54f23ffe4747e2c17660b6bc804ff9d941c712
Biao <biao.mac@icloud.com>
2019-06-17 17:16:49 +0800

    circle

--- mix/src/main/java/RandomTest.java
+++ mix/src/main/java/RandomTest.java
@@ -1,3 +1,5 @@
+import util.Random;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
--- mix/src/main/java/Test.java
+++ mix/src/main/java/Test.java
@@ -1,26 +1,25 @@
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Test {
-    // 求 x 的 n 次方
-    static int foo(int x, int n) {
-        if (n == 0) { return 1; }
-        return foo(x*x, n/2) * (n%2==0 ? 1 : x);
-    }
-
-    static int pow(int x, int n, int t) {
-        if (n == 0) { return 1; }
-        if (n == 1) { return x * t; }
-        return pow(x*x, n/2, t*(n%2==0 ? 1 : x));
-    }
-
     public static void main(String[] args) throws Exception {
-        System.out.println(pow(3, 1, 1));
-        System.out.println(pow(3, 4, 1));
-        System.out.println(pow(3, 5, 1));
-        System.out.println(pow(3, 11, 1));
-        System.out.println(foo(3, 5));
-        System.out.println(foo(3, 11));
+        double r = 100;
+        int n = 16;
+        double x = aroundCircleRadius(r, n);
+
+        System.out.println(x);
+        System.out.println(2*Math.PI*(r+x));
+        System.out.println(2*n*x);
+    }
+
+    /**
+     * 计算紧紧围绕半径为 r 的大圆的 n 个小圆的半径 (大圆和小圆相切，小圆之间相切)
+     *
+     * @param r 大圆的半径
+     * @param n 小圆的个数
+     * @return 返回小圆的半径
+     */
+    public static double aroundCircleRadius(double r, int n) {
+        // 圆的内切正 n 边型的边长为 2*r*sin(π/n)
+        // 小圆的半径为 x, 计算半径为 r+x 的圆的 2n 个内切正多边形的边长 b, b 等于 x
+        double t = Math.sin(Math.PI/2/n);
+        return (2*r*t) / (1-2*t);
     }
 }
