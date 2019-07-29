622b9dc9caa9ee81b42f82c6c3950ba7fd05ee41
Biao <biao.mac@icloud.com>
2019-07-29 17:35:22 +0800

    java

--- mix/src/main/java/FFmpegX.java
+++ mix/src/main/java/FFmpegX.java
@@ -1,15 +1,22 @@
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFmpegUtils;
 import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.job.FFmpegJob;
 import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 import net.bramp.ffmpeg.probe.FFmpegStream;
+import net.bramp.ffmpeg.progress.Progress;
+import net.bramp.ffmpeg.progress.ProgressListener;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class FFmpegX {
     static FFmpeg ffmpeg;
 
@@ -43,6 +50,12 @@ public class FFmpegX {
         }
     }
 
+    /**
+     * 提取 MP4 视频的封面
+     *
+     * @param mp4 MP4 文件
+     * @throws IOException
+     */
     public static void extractCover(File mp4) throws IOException {
         File cover = new File(mp4.getParent(), String.format("cover-%s.png", FilenameUtils.getBaseName(mp4.getName())));
 
@@ -55,20 +68,61 @@ public class FFmpegX {
                 .setVideoFilter("select='gte(t\\,5)'") // 第 5 秒的画面
                 .done();
 
-
         FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
         executor.createJob(builder).run();
     }
 
+    /**
+     * 音频文件转换为 mp3
+     *
+     * @param audio 音频文件
+     * @throws IOException
+     */
+    public static void convertMp3(File audio) throws IOException {
+        File mp3 = new File(audio.getParent(), "out.mp3");
+        FFmpegProbeResult in = ffprobe.probe(audio.getAbsolutePath());
+
+        FFmpegBuilder builder = new FFmpegBuilder()
+                .setInput(audio.getAbsolutePath())
+                .overrideOutputFiles(true)
+                .addOutput(mp3.getAbsolutePath())
+                .done();
+
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+        FFmpegJob job = executor.createJob(builder, new ProgressListener() {
+            // 使用 FFmpegProbeResult 得到视频的长度 (单位为纳秒)
+            final double duration_ns = in.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
+
+            @Override
+            public void progress(Progress progress) {
+                // 转换进度 [0, 100]
+                // [Fix] No duration for FLV, SWF file, 所以获取进度无效时都假装转换到了 99%
+                int percentage = (duration_ns > 0) ? (int)(progress.out_time_ns / duration_ns * 100) : 99;
+
+                // 日志中输出转换进度信息
+                log.info("[{}%] status: {}, frame: {}, time: {} ms, fps: {}, speed: {}x",
+                        percentage,
+                        progress.status,
+                        progress.frame,
+                        FFmpegUtils.toTimecode(progress.out_time_ns, TimeUnit.NANOSECONDS),
+                        progress.fps.doubleValue(),
+                        progress.speed
+                );
+            }
+        });
+        job.run();
+    }
+
     public static void main(String[] args) throws IOException {
-        probe();
-        extractCover(new File("/Users/Biao/Desktop/238855614619451392.mp4"));
-        extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
-        extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
-        extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
-        extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
-        extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
-        extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
-        extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
+        // probe();
+        // extractCover(new File("/Users/Biao/Desktop/238855614619451392.mp4"));
+        // extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
+        // extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
+        // extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
+        // extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
+        // extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
+        // extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
+        // extractCover(new File("/Users/Biao/Desktop/Tears in Heaven.mp4"));
+        convertMp3(new File("/Users/Biao/Desktop/error-cx.mp3"));
     }
 }
---
+++ mix/src/main/java/Test.java
@@ -0,0 +1,7 @@
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(5 & ~1);
+    }
+}
