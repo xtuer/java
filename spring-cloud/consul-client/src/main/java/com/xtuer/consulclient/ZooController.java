cb3594a80d9d7a93e0437e2e6f0657290960703f
Biao <biao.mac@icloud.com>
2020-02-03 20:32:23 +0800

    -_-

---
+++ spring-cloud/consul-client/src/main/java/com/xtuer/consulclient/ZooController.java
@@ -0,0 +1,17 @@
+package com.xtuer.consulclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ZooController {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        return discoveryClient.getServices().toString();
+    }
+}
