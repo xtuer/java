be1f94db5c134f6d247cfe3a1c5d302a50d76496
Biao <biao.mac@icloud.com>
2020-06-15 16:54:27 +0800

    -_-

--- exam/exam-web-app/src/main/java/com/xtuer/bean/CacheConst.java
+++ exam/exam-web-app/src/main/java/com/xtuer/bean/CacheConst.java
@@ -4,7 +4,9 @@ package com.xtuer.bean;
  * 缓存使用的键和缓存的名字
  */
 public interface CacheConst {
-    String CACHE = "xtuer:"; // 默认的缓存对象
+    // 默认的缓存对象，针对每一个缓存进行统计: CacheManager -> Cache -> CacheElement
+    // 如果要做的更细致，不同的业务使用不同的 Cache 对象
+    String CACHE = "xtuer:";
 
     // 用户机构
     String KEY_USER_ID  = "'user.' + #userId";
